760bbb41356921d326f484d47ebfdf6d94e47ad7
Arpit Aggarwal <arpitaggarwal0511@gmail.com>
2025-08-01 16:33:58 +0530

    env

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -15,10 +15,11 @@ export default function Home() {
     formData.append("file", file);
 
     try {
-      const res = await fetch("http://localhost:8000/analyze", {
-        method: "POST",
-        body: formData,
-      });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/analyze`, {
+    method: "POST",
+    body: formData,
+  });
+
 
       const data = await res.json();
       setResult(data);
